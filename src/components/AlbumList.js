6b9031a5e56c549108a0bebb6b85ed22a0267c90
mwMajch <mw.majch@gmail.com>
2018-07-04 16:31:48 +0200

    first commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+//import axios from 'axios';
+
+class AlbumList extends Component {
+componentWillMount() {
+     fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => response.json())
+    .then((responseData) => {
+        console.log(responseData);
+    });
+}
+
+    render() {
+        return ( 
+             <View>
+                 <Text> Album List </Text>
+            </View>    
+        ); 
+    } 
+}
+export default AlbumList;
